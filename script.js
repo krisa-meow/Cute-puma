25c8f3d0c9b782b3bdfa045b5327a517c6620bb0
Krisa-meow <grayficus043@gmail.com>
2026-02-27 18:50:59 +0300

    #

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const datepicker = document.querySelector('.datepicker');
-const rangeInput = datepicker.querySelector('input');
-const calendarContainer = datepicker.querySelector('.calendar');
+const datepicker = document.querySelector(".datepicker");
+const rangeInput = datepicker.querySelector("input");
+const calendarContainer = datepicker.querySelector(".calendar");
+const leftCalendar = datepicker.querySelector(".left-side");
+const rightCalendar = datepicker.querySelector(".right-side");
+
+let leftDate = new Date();
+let rightDate = new Date(leftDate);
+rightDate.setMonth(rightDate.getMonth() + 1);
+
+calendarContainer.hidden = false;
+
+const renderCalendar = (calendar, year, month) => {
+    const label = calendar.querySelector(".label");
+    label.textContent = new Date(year, month).toLocaleString(
+        navigator.language || "en-US",
+        {
+            year: "numeric",
+            month: "long",
+        }
+    );
+
+    const datesContainer = calendar.querySelector(".dates");
+    datesContainer.innerHTML = "";
+};
+
+renderCalendar(leftCalendar, leftDate.getFullYear(), leftDate.getMonth());
+renderCalendar(rightCalendar, rightDate.getFullYear(), rightDate.getMonth());
 
 // show datepicker
 rangeInput.addEventListener("focus", () => {
